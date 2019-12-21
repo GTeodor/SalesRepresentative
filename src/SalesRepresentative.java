5e54d2f1b6001039eb81a70d8529a3bd0837640f
Muresan Teodor <teodor@test.com>
2019-12-21 17:00:58 +0200

    version 1

--- src/SalesRepresentative.java
+++ src/SalesRepresentative.java
@@ -1,2 +1,40 @@
+/**
+ * This is a SalesRepresentative data type class
+ */
 public class SalesRepresentative {
+    private int numberOfSales;
+    private double quotaPerSale;
+    private String name;
+
+    /**
+     * Construct and initializes a SalesRepresentative
+     * @param name the name of the sales representative
+     * @param numberOfSales the number of sales
+     * @param quotaPerSales the quota per sales
+     */
+    public SalesRepresentative(String name, int numberOfSales, double quotaPerSales){
+        this.numberOfSales = numberOfSales;
+        this.quotaPerSale = quotaPerSales;
+        this.name = name;
+    }
+
+    /**
+     * This method computes the worth of sales as
+     * a product of number of sales and quota per sale
+     * @return the worth of sales
+     */
+    public double wortOfSales () {
+        return numberOfSales * quotaPerSale;
+    }
+
+
+    /**
+     *  This method override toString method
+     * @return a string with a name, number of sales and quota per sale
+     */
+
+    @Override
+    public String toString() {
+        return "Sales " + name +" has " + numberOfSales +" sales with a quota of " + quotaPerSale +"$. He has achieved " + wortOfSales() +"$ worth of sales.";
+    }
 }
--- src/SalesRepresentativeUtil.java
+++ src/SalesRepresentativeUtil.java
@@ -1,2 +1,47 @@
+/**
+ * This class is developed for executing a bubble sort algorithm
+ * on objects of SalesRepresentative data type
+ */
+
 public class SalesRepresentativeUtil {
+    public static void main(String[] args) {
+
+        SalesRepresentative guy1 = new SalesRepresentative("guy 1",10, 500);
+        SalesRepresentative guy2 = new SalesRepresentative("guy 2",7, 800);
+        SalesRepresentative guy3 = new SalesRepresentative("guy 3",5,400);
+        SalesRepresentative guy4 = new SalesRepresentative("guy 4",7,1000);
+
+
+        SalesRepresentative [] reps = {guy1, guy2,guy3,guy4};
+        SalesRepresentative [] sortedReps = SalesRepresentativeUtil.sort(reps);
+
+        for (SalesRepresentative salesRepresentative: sortedReps) {
+            System.out.println(salesRepresentative);
+        }
+    }
+
+    /**
+     * This method is develop to sort an array of objects of
+     * SalesRepresentative data type using a bubble sort algorithm
+     *
+     * @param reps will be the name of the arrays of object
+     * @return the sorted array in a descending order with
+     * the most revenue generated via sales the first object
+     */
+
+    public static SalesRepresentative [] sort(SalesRepresentative[] reps) {
+        int n = reps.length;
+        SalesRepresentative temp;
+        for(int i=0; i < n; i++){
+            for(int j=1; j < (n-i); j++){
+                if(reps[j-1].wortOfSales() < reps[j].wortOfSales()){
+                    // Swap elements
+                    temp = reps[j-1];
+                    reps[j-1] = reps[j];
+                    reps[j] = temp;
+                }
+            }
+        }
+        return reps;
+    }
 }
